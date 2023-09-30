7b3821bd18419abbe7057671d3849816fc54f778
CuongNguyen <22028327@vnu.edu.vn>
2023-09-30 23:39:07 +0700

    Add resource file

--- src/Application/java/DictionaryApplication.java
+++ src/Application/java/DictionaryApplication.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
--- src/Application/java/SceneController.java
+++ src/Application/java/SceneController.java
@@ -1,6 +1,23 @@
 package Application.java;
 
 import javafx.fxml.FXML;
-public class SceneController {
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 
+public class SceneController {
+    public void switchScene(MouseEvent event) throws IOException {
+        RootManager rootManager = new RootManager();
+        Parent root = rootManager.getFXMLInsertedRoot("DictionaryApplication.fxml");
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); /** get the source node that generated this event,
+         since every node class inherits Node, this typecasting is valid, from this node, get the corresponding scene then from
+         the scene, we get the window*/
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
