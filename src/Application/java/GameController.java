a16957e35919dacda622d179690de1e24cc897e8
CuongNguyen <22028327@vnu.edu.vn>
2023-11-15 16:57:39 +0700

    Juan

--- src/Application/java/GameController.java
+++ src/Application/java/GameController.java
@@ -1,36 +1,49 @@
 package Application.java;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.text.FontWeight;
 
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class GameController {
-    @FXML
-    private Label wordLabel;
-    @FXML
-    private TextField answerField;
-    @FXML
-    private Label resultLabel;
+public class GameController implements Initializable {
 
-    private String currentWord;
-    private String currentAnswer;
+    @FXML
+    private Label questionLabel;
+    @FXML
+    private Button answerButton1;
+    @FXML
+    private Button answerButton2;
+    @FXML
+    private Button answerButton3;
+    @FXML
+    private Button answerButton4;
+    @FXML
+    private MediaView timerMediaView;
 
-    public void initialize() {
-        currentWord = "Apple";
-        currentAnswer = "apple";
-        wordLabel.setText(currentWord);
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        FXMLManager fxmlManager = new FXMLManager();
+        questionLabel.setFont(fxmlManager.cloneQuicksandFont(FontWeight.BOLD, 18));
+        answerButton1.fontProperty().bind(questionLabel.fontProperty());
+        answerButton2.fontProperty().bind(questionLabel.fontProperty());
+        answerButton3.fontProperty().bind(questionLabel.fontProperty());
+        answerButton4.fontProperty().bind(questionLabel.fontProperty());
+        initTimerMediaView();
     }
 
-    @FXML
-    private void checkAnswer() {
-        String userAnswer = answerField.getText().trim().toLowerCase();
-
-        if (userAnswer.equals(currentAnswer)) {
-            resultLabel.setText("Correct!");
-        } else {
-            resultLabel.setText("Incorrect. Try again.");
-        }
+    private void initTimerMediaView() {
+        Media media = new Media(new File("src\\Application\\resources\\Animation\\Countdown.mp4").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        timerMediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.play();
     }
 }
